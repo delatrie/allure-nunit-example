351157e8aefb5d6534a180a7ac864a25bb8f5204
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-09-26 02:32:45 +0700

    Create snexe.ps1

---
+++ snexe.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+Param ()
+
+If (-not $IsWindows)
+{
+    Write-Error "Can't run this script on $($PSVersionTable.Platform). Windows is required"
+    Return
+}
+
+$VsInstallPath = vswhere.exe -latest -property installationPath
+$LaunchDevToolPath = Join-Path $VsInstallPath 'Common7\Tools\Launch-VsDevShell.ps1'
+
+if (-not (Test-Path -PathType Leaf $LaunchDevToolPath))
+{
+    Write-Error "Can't find the Developer PowerShell launch script at '$LaunchDevToolPath'"
+    Return
+}
+
+$SnExe = (Get-Command -CommandType Application -ErrorAction SilentlyContinue "sn.exe").Source
+If (-not $SnExe)
+{
+    Write-Error "Can't find sn.exe"
+    Return
+}
+
+$SnExe
