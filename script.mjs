4e35a6be2e16f6336e3625fbab3e66c397ebed95
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-12-12 11:51:22 +0700

    Refactor PowerShell command execution in script.mjs
    Removed output encoding setting and console logs for exit code, signal, stdout, and stderr.

--- script.mjs
+++ script.mjs
@@ -8,7 +8,7 @@ const args = [
   "-NoProfile",
   "-NonInteractive",
   "-Command",
-  "& { [System.Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($False, $False); Write-Output '♪♪♪'; Get-CimInstance -Class Win32_Process | Select-Object @{ n='ppid'; e={ $_.ParentProcessId } },@{ n='pid'; e={ $_.ProcessId } },@{ n='comm'; e={ $_.ExecutablePath } } | Format-Table -AutoSize ppid,pid,comm }",
+  "& { [System.Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($False, $False); Get-CimInstance -Class Win32_Process | Select-Object @{ n='ppid'; e={ $_.ParentProcessId } },@{ n='pid'; e={ $_.ProcessId } },@{ n='comm'; e={ $_.ExecutablePath } } | Format-Table -AutoSize ppid,pid,comm }",
 ];
 
 const proc = spawn("powershell.exe", args, { stdio: ["ignore", "pipe", "pipe"], shell: false, timeout: 20000 });
@@ -16,12 +16,7 @@ proc.stdout.setEncoding("utf-8").on("data", (data) => { stdout.push(data); });
 proc.stderr.setEncoding("utf-8").on("data", (data) => { stderr.push(data); });
 await new Promise((resolve) => {
   proc.on("exit", (code, signal) => {
-    console.log("exit code", code);
-    console.log("signal", signal);
-    console.log("stdout:");
-    console.log(stdout.join("").replace(/(\r?\n)/, "|$1"));
-    console.log("stderr:");
-    console.log(stderr.join(""));
+    console.log(stdout.join("").split(/(\r?\n)/));
     resolve();
   });
 });
