293804dc9839a075575fd894fe8b3f84ee6f929c
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-12-12 11:26:10 +0700

    Create script.mjs

---
+++ script.mjs
@@ -0,0 +1,27 @@
+import { spawn } from "node:child_process";
+
+const stdout = [];
+const stderr = [];
+
+const args = [
+  "-NoLogo",
+  "-NoProfile",
+  "-NonInteractive",
+  "-Command",
+  "& { Get-CimInstance -Class Win32_Process | Select-Object @{ n='ppid'; e={ $_.ParentProcessId } },@{ n='pid'; e={ $_.ProcessId } },@{ n='comm'; e={ $_.ExecutablePath } } | Format-Table ppid,pid,comm }",
+];
+
+const proc = spawn("powershell.exe", args, { stdio: ["ignore", "pipe", "pipe"], shell: false, timeout: 20000 });
+proc.stdout.setEncoding("utf-8").on("data", (data) => { stdout.push(data); });
+proc.stderr.setEncoding("utf-8").on("data", (data) => { stderr.push(data); });
+await new Promise((resolve) => {
+  proc.on("exit", (code, signal) => {
+    console.log("exit code", code);
+    console.log("signal", signal);
+    console.log("stdout:");
+    console.log(stdout.join(""));
+    console.log("stderr:");
+    console.log(stderr.join(""));
+    resolve();
+  });
+});
